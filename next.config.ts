477ad8fd33e69164387d624f838fec34822c1b7c
태민석 <xoals3497@naver.com>
2025-11-06 18:32:52 +0900

    CLARIFAI 모듈 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['clarifai-nodejs-grpc'],
+  },
 };
 
 export default nextConfig;
